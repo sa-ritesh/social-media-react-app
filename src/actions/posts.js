a898774ee4e649c5f3a93f41b0b2728129231178
Ritesh <arora.ritesh.95@gmail.com>
2021-03-29 12:13:52 +0530

    Router SetUp

---
+++ src/actions/posts.js
@@ -0,0 +1,22 @@
+import { UPDATE_POSTS } from "./actionTypes";
+
+export function fetchPosts() {
+  return (dispatch) => {
+    const url = "http://codeial.abhishekgarg.tech/api/v1/posts";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch(updatePosts(data.data.posts));
+      });
+  };
+}
+
+export function updatePosts(posts) {
+  return {
+    type: UPDATE_POSTS,
+    posts,
+  };
+}
